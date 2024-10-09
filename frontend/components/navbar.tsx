b0368e80d8fe92ed63b644395795782411a7ef9a
Elangeswaran <helangeswaran@ad.bradley.edu>
2024-10-09 09:35:48 -0500

    optimal solution page + navbar activation

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,7 +1,14 @@
+'use client'; // This marks the component as a Client Component
+
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const pathname = usePathname(); // Hook for getting the current path
+
+  const isActive = (path: string) => pathname === path;
+
   return (
     <nav className='px-24 py-4 flex items-center justify-between gap-x-5 border-b-2 bg-gray-900'>
       <Link href='/' className='flex items-center gap-x-2'>
@@ -18,21 +25,28 @@ const Navbar = () => {
           alt='Picture of the author'
           className='-mt-[2px]'
         />
-        {/* <p className='text-white text-2xl font-semibold'>Configable</p> */}
       </Link>
       <ul className='flex gap-x-6 text-xl'>
-        {/* <li>
+        <li>
           <Link
-            href='/tool'
-            className='font-medium text-white cursor-pointer border-b border-transparent hover:border-b hover:border-white'
+            href='/optimal-tool'
+            className={`cursor-pointer border-b border-transparent ${
+              isActive('/optimal-tool')
+                ? 'text-sky-400 border-sky-400'
+                : 'text-white hover:text-sky-400'
+            }`}
           >
-            Tool
+            Optimal Tool
           </Link>
-        </li> */}
+        </li>
         <li>
           <Link
             href='/details'
-            className='text-white hover:text-sky-400 cursor-pointer border-b border-transparent'
+            className={`cursor-pointer border-b border-transparent ${
+              isActive('/details')
+                ? 'text-sky-400 border-sky-400'
+                : 'text-white hover:text-sky-400'
+            }`}
           >
             Details
           </Link>
@@ -40,7 +54,11 @@ const Navbar = () => {
         <li>
           <Link
             href='/team'
-            className='text-white hover:text-sky-400 cursor-pointer border-b border-transparent'
+            className={`cursor-pointer border-b border-transparent ${
+              isActive('/team')
+                ? 'text-sky-400 border-sky-400'
+                : 'text-white hover:text-sky-400'
+            }`}
           >
             Team
           </Link>
